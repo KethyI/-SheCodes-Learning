d15e8dfba635b6df83f6ed7c1d1bcb8144660b16
KethyI <kot1256@gmail.com>
2023-05-17 05:58:56 +0300

    added shecodes API

--- weather app/src/app.js
+++ weather app/src/app.js
@@ -20,6 +20,27 @@ let searchLine = document.querySelector("#search-line");
 let chosenCity = document.querySelector("#search-line").value;
     
 function showTemp(response){
+    let chosenCity = response.data.city;
+    let cityShown = document.querySelector("#city-now");
+    let tempShown = document.querySelector("#temp-number");
+    let roundTemp = Math.round(response.data.temperature.current);
+    let tempNow = roundTemp;
+    let clouds = document.querySelector("#cloudiness");
+    let currentClouds = response.data.condition.description;
+    let humidity = document.querySelector("#humidity")
+    let currentHumidity = response.data.temperature.humidity;
+    let wind = document.querySelector("#wind")
+    let currentWind = Math.round(response.data.wind.speed);
+
+    clouds.innerHTML = currentClouds;
+    cityShown.innerHTML = chosenCity;
+    tempShown.innerHTML = tempNow;
+    humidity.innerHTML = currentHumidity;
+    wind.innerHTML = currentWind;
+
+}
+
+function CurrentLocationTemp(response){
     let chosenCity = response.data.name;
     let cityShown = document.querySelector("#city-now");
     let tempShown = document.querySelector("#temp-number");
@@ -39,49 +60,19 @@ function showTemp(response){
     wind.innerHTML = currentWind;
 
 }
-
-function showStartTemp(response){
-    let chosenCity = response.data.name;
-    let cityShown = document.querySelector("#city-now");
-    let tempShown = document.querySelector("#temp-number");
-    let roundTemp = Math.round(response.data.main.temp);
-    let tempNow = roundTemp;
-    let clouds = document.querySelector("#cloudiness");
-    let currentClouds = response.data.weather[0].main;
-    let humidity = document.querySelector("#humidity")
-    let currentHumidity = response.data.main.humidity;
-    let wind = document.querySelector("#wind")
-    let currentWind = Math.round(response.data.wind.speed);
-
-    clouds.innerHTML = currentClouds;
-    cityShown.innerHTML = chosenCity;
-    tempShown.innerHTML = tempNow;
-    humidity.innerHTML = currentHumidity;
-    wind.innerHTML = currentWind;
-
-}
-
-function getLatLon(response){
-    let apiKey = "c819171fe0abdc14039af4ef5dda283b";
-    let lat = response.data[0].lat;
-    let lon = response.data[0].lon;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
-    axios.get(apiUrl).then(showTempCurrent);
-   }
-
-   function getLatLonStart(response){
-    let apiKey = "c819171fe0abdc14039af4ef5dda283b";
-    let lat = response.data[0].lat;
-    let lon = response.data[0].lon;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
-    axios.get(apiUrl).then(showStartTemp);
-   }
 
 function startSearch(event) {
     event.preventDefault();
 
     let chosenCity = document.querySelector("#search-line").value;
- search(chosenCity);
+    let cityShown = document.querySelector("#city-now");
+
+    cityShown.innerHTML = chosenCity;
+
+    let apiKey = "af9f195dtc3b2336169c4ob0f8c90052";
+    let apiCity = `https://api.shecodes.io/weather/v1/current?query=${chosenCity}&key=${apiKey}&units=metric`
+    axios.get(apiCity).then(showTemp);
+
 }
 
 function showTempCurrent(response) {
@@ -111,8 +102,7 @@ function displayCurrentWeather(position) {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
     let apiUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
-    
-    axios.get(apiUrlCurrent).then(showTemp);
+    axios.get(apiUrlCurrent).then(CurrentLocationTemp);
 
 }
 
@@ -123,27 +113,15 @@ function searchLocation(event) {
 
 }
 
-function search(city) {
-   
-    let cityShown = document.querySelector("#city-now");
-
-    cityShown.innerHTML = city;
-
-    let apiKey = "c819171fe0abdc14039af4ef5dda283b";
-    let apiCity = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
-    axios.get(apiCity).then(getLatLon);
-
-}
-
 function searchStart(city) {
    
     let cityShown = document.querySelector("#city-now");
 
     cityShown.innerHTML = city;
 
-    let apiKey = "c819171fe0abdc14039af4ef5dda283b";
-    let apiCity = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
-    axios.get(apiCity).then(getLatLonStart);
+    let apiKey = "af9f195dtc3b2336169c4ob0f8c90052";
+    let apiCity = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+    axios.get(apiCity).then(showTemp);
 
 }
 
@@ -154,6 +132,25 @@ let currentLocationButton = document.querySelector("#current-weather-button");
 currentLocationButton.addEventListener("click", searchLocation);
 
 searchStart("Kyiv");
+
+// function getLatLon(response){
+//     let apiKey = "c819171fe0abdc14039af4ef5dda283b";
+//     let lat = response.data[0].lat;
+//     let lon = response.data[0].lon;
+//     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+//     axios.get(apiUrl).then(showTempCurrent);
+//    }
+
+//    function getLatLonStart(response){
+//     let apiKey = "c819171fe0abdc14039af4ef5dda283b";
+//     let lat = response.data[0].lat;
+//     let lon = response.data[0].lon;
+//     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+//     axios.get(apiUrl).then(showStartTemp);
+//    }
+
+
+
 // let celsius = document.querySelector("#celsius");
 // function showCelsius() {
 //     let celsiusNumber = document.querySelector("#temp-number");
